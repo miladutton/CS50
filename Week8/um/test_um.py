09080c4aaaf7affd1391d928eb5a84477c5a2c50
Mila Dutton <mila.dutton@gmail.com>
2025-01-15 15:39:26 -0800

    Create test_um.py

---
+++ Week8/um/test_um.py
@@ -0,0 +1,9 @@
+import pytest
+from um import count
+
+def test_um():
+    assert count("um") == 1
+    assert count("um?") == 1
+    assert count("Um, thanks for the album.") == 1
+    assert count("Um, thanks, um...") == 2
+    assert count("Summer is almost here") == 0
